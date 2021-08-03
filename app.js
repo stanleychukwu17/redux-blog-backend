643b5fb94b06e0c4019eb2acc7f6240acfe817e5
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-08-03 09:01:37 +0100

    changes

--- app.js
+++ app.js
@@ -60,8 +60,11 @@ app.get('/blogs/all-blogs', (req, res, next) => {
 });
 
 // for returning of one blog
-app.get('/blogs/one-blog/:id', (req, res, next) => {
-    console.log(req.params.id)
+app.get('/blogs/one-blog/:id', async (req, res, next) => {
+    const blog_id = req.params.id;
+
+    const blogDts = await BlogsModel.findById(blogId).exec();
+    console.log(req.params.id, blogDts);
     res.json({'msg':'okay'});
 });
 
