cdd809a6fc6e7711693ef36059b8bd6df05dabf3
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-08-08 16:02:45 +0100

    changes

---
+++ models/Bcomments.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const BlogsComments = mongoose.Schema({
+  blogId: {type: String, trim: true},
+  userId: {type: String, trim: true},
+  comment: {type: String, trim: true}
+});
+
+module.exports = mongoose.model('blogs_comments', BlogsComments);
