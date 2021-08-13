78035af972bd6a5c395750c9028d943c3248d4e1
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-08-13 08:12:55 +0100

    changes

--- app.js
+++ app.js
@@ -118,6 +118,9 @@ app.post('/blogs/makeComment/', async (req, res, next) => {
 
 // for deleting of comments on a blog post
 app.post('/blogs/deleteComment', async (req, res, next) => {
+    const blogs_comments = require('./models/Bcomments')
+    const  {comId, userId} = req.body
+
     res.json({'msg':'okay'})
 })
 
