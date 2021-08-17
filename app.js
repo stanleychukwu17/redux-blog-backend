31efe6f9d5372aa89e31c21dd1db57302a03e017
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-08-17 21:56:50 +0100

    changes

--- app.js
+++ app.js
@@ -128,7 +128,9 @@ app.post('/blogs/deleteComment', async (req, res, next) => {
 
 
 // for fetching of the activities section at the side of the page
-app.post('/activities', async (req, res, next) => {})
+app.post('/activities', async (req, res, next) => {
+    
+})
 
 // for users to login into their accounts
 app.post('/users/login', (req, res, next) => {
