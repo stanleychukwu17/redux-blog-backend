e209ada27339adb33831f13ffd109252aed1efa9
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-08-18 17:21:21 +0100

    changes

--- pumba/ut.js
+++ pumba/ut.js
@@ -1,12 +1,19 @@
 const LmD = require('../models/BlogLikesModel')
 const blogs_comments = require('../models/Bcomments')
 const all_activities = require('../models/aActivities')
+const UserModel = require('./models/UserModel')
 
 const bt = {
     get_likes_of_this_blog : async (blogId = 0) => {
         const bcur = await LmD.blogs_ech_likes.find({blog: blogId}).exec(); // searches the mongodb collection for the blog likes
         const likes = (bcur.length > 0) ? (bcur[0].likes) : 0;
         return likes
+    },
+
+    // using the id of a user to fetch the username of that user
+    get_this_user_details : async (userId) => {
+        const buser = await UserModel.findById(userId, 'username').exec();
+        return {userId, 'name':buser.username}
     },
 
     // gets the total comments on a blog using the id received
