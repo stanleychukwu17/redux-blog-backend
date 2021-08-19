71e4790b6175bafe15f5a2e249d6e2e1c6672d8d
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-08-19 21:21:27 +0100

    changes

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ app.get('/activities/getActivities/', async (req, res, next) => {
     aDts.forEach(async (ab) => {
         const {wch, id1:userId, id2, id3} = ab;
 
-        // fetch the details of the user
+        // fetch the details of the user who added the activity
         const {name} = await utFunc.get_this_user_details(userId);
 
         // arrange the link for the new blog, put the statement out
